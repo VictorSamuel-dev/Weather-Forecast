e34fe136d71909ff2f01897fae1f92e086b5cecd
VictorSamuel-dev <142364059+VictorSamuel-dev@users.noreply.github.com>
2023-10-18 01:55:02 -0400

    Modified HTML, CSS, JS, and added ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Weather-Forecast
+
+## User Story
+Creating a travel forcast for the next 5 days. When you search the city you are going to, you get up-to-date forcast on the weather.
+
+## Applications
+ - [API](https://openweathermap.org/api)
+ - [Fonts](https://fonts.google.com/)
+ - HTML
+ - JS
+ - CSS
+
+ ## Images
+ - [Screenshot](./assets/images/Trial.png)
+
+ ## Demo
+ - [WeatherApp](https://www.loom.com/share/bd71ae8858a54e71ac93c643926ccea1?sid=d38e0b0e-a2a4-4c26-bead-74c110209110)
+
+ ## Deployed
+    *()
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,175 @@
-const cityInput = document.querySelector(".city-input");
-const searchButton = document.querySelector(".search-btn");
-const weatherCardsDiv = document.querySelector(".weather-cards");
+const apiKey = 'd48e73daf0593c92a7af6d1e2c94472a';
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+const currentWeather = document.getElementById('current-weather');
+const forecast = document.getElementById('forecast');
+const searchHistory = document.getElementById('search-history');
 
-const API_KEY = "d48e73daf0593c92a7af6d1e2c94472a"; // API key for OpenWeatherMap API
+// Load search history from local storage
+const storedSearchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-const createWeatherCard = (weatherItem) => {
-    return  ` <li class="card">
-                <h3>(${weatherItem.dt.txt.split(" ")[0]})</h3>
-                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}10d@2x.png" alt="weather-icon">
-                <h4>Temperature: ${(weatherItem.main.temp)}°F</h4>
-                <h4>Wind: ${weatherItem.wind.speed} MPH</h4>
-                <h4>Humidity: ${weatherItem.main.humidity}%</h4>    
-            </li>`;
-}
+// Render search history from local storage
+renderSearchHistory();
 
-const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${API_key}`;
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const cityName = cityInput.value;
+    fetchWeatherData(cityName);
+});
 
-    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-
-        // Filter the forecasts to get only one forecast per day
-        const uniqueForecastDays = [];
-        const fiveDaysForecast = data.list.filter(forecast => {
-            const forecastDate = new Date(forecast.dt_txt).getDate();
-            if(!uniqueForecastDays.includes(forecastDate)) {
-                return uniqueForecastDays.push(forecastDate);
-            }
+function fetchWeatherData(cityName) {
+    // Make an API request to OpenWeatherMap
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Process and display current weather data
+            displayCurrentWeather(data);
+            // Store the city in search history
+            addToSearchHistory(cityName);
+            // Fetch 5-day forecast
+            fetchForecast(cityName);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
         });
+}
 
-        // Clearing previous weather data
-        cityInput.value = "";
-        weatherCardsDiv.innerHTML = "";
+function displayCurrentWeather(data) {
+    // Display current weather data in the currentWeather element
+    // Extract and format the necessary information from the 'data' object
+    currentWeather.innerHTML = `<h2>${data.name}</h2>
+                               <p>Date: ${new Date(data.dt * 1000).toLocaleDateString()}</p>
+                               <p>Temperature: ${data.main.temp}°C</p>
+                               <p>Humidity: ${data.main.humidity}%</p>
+                               <p>Wind Speed: ${data.wind.speed} m/s</p>`;
+}
 
-        console.log(fiveDaysForecast);
-        fiveDaysForecast.forEach(weatherItem => {
-            weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
-            
+function addToSearchHistory(cityName) {
+    // Add the city name to the search history
+    storedSearchHistory.push(cityName);
+    // Store the updated search history in local storage
+    localStorage.setItem('searchHistory', JSON.stringify(storedSearchHistory));
+    // Render the updated search history
+    renderSearchHistory();
+}
+
+function renderSearchHistory() {
+    // Render search history from local storage
+    searchHistory.innerHTML = '';
+    for (const cityName of storedSearchHistory) {
+        const historyItem = document.createElement('div');
+        historyItem.textContent = cityName;
+        searchHistory.appendChild(historyItem);
+        // Add click event to historyItem to fetch weather for that city
+        historyItem.addEventListener('click', function () {
+            fetchWeatherData(cityName);
         });
-    }).catch(() => {
-        alert("An error occurred while fetching the weather forcast!");
-    });
+    }
 }
 
-const getCityCoordinates = () => {
-    const cityName = cityInput.value.trim(); // Get user entered city name and removed extra spaces
-    if(!cityName) return; //Return if cityName is empty
-    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_key}`;
-
-    // Get entered city coordinates (latitude, longitude, and name) from the API response
-    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-        if(!data.length) return alert('No coordinates found for ${cityName}');  //console.log(data)
-        const { name, lat, lon } = data[0];
-        getWeatherDetails(name, lat, lon);
-    }).catch(() => {
-        alert("An error occurred while fetching the coordinates!");
-    });
+function fetchForecast(cityName) {
+    // Make an API request to get a 5-day forecast
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+    fetch(forecastUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Process and display the 5-day forecast
+            displayForecast(data);
+        })
+        .catch(error => {
+            console.error('Error fetching forecast data:', error);
+        });
 }
 
-searchButton.addEventListener("click", getCityCoordinates);
+function displayForecast(data) {
+    // Display the 5-day forecast data in the forecast element
+    // Extract and format the necessary information from the 'data' object
+    // Iterate over the 'list' array to get data for each day
+    forecast.innerHTML = ''; // Clear previous forecast data
+    const forecastItems = []
+    forecast.innerHTML += `<h2>5-Day Forecast</h2>`;
+
+    for (let i = 0; i < 5; i++) {
+        const forecastItem = data.list[i];
+        const dateUnix = forecastItem.dt;
+        const fahrenheitTemperature = ((forecastItem.main.temp - 273.15) * 9/5) + 32;
+        const date = new Date ((dateUnix + ((i + 1) * 24 * 60 * 60)) * 1000)
+        const windSpeedMPH = (forecastItem.wind.speed * 2.24).toFixed(2);
+
+    forecastItems.push(`
+        <li class="card">
+            <h3>${date.toLocaleDateString()}</h3>
+            <h4>Temperature: ${fahrenheitTemperature.toFixed(2)}°F</h4>
+            <h4>Wind: ${windSpeedMPH}MPH</h4>
+            <h4>Humidity: ${forecastItem.main.humidity}%</h4>
+        </li>`)
+    }
+
+    forecast.innerHTML += `<ul class="weather-cards">
+    ${forecastItems.join(" ")}
+    </ul>`
+}
+
+const createWeatherCard = (cityName, weatherItem) => {
+    if (index == 0) { //HTML for the main weather card
+        return ` <div class="details">
+                    <h2>${cityName} (${weatherItem.split(" ")[0]})</h2>
+                    <h4>Temperature: ${(weatherItem.main.temp)}°F</h4>
+                    <h4>Wind: ${weatherItem.wind.speed} MPH</h4>
+                    <h4>Humidity: ${weatherItem.main.humidity}%</h4>   
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}10d@4x.png" alt="weather-icon">
+                    <h4>${weatherItem.weather[0].description}</h4>
+                </div>`;
+
+    } else { //HTML for the other five day forecast card
+        return `<li class="card">
+                    <h3>(${weatherItem.split(" ")[0]})</h3>
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}10d@2x.png" alt="weather-icon">
+                    <h4>Temperature: ${(weatherItem.main.temp)}°F</h4>
+                    <h4>Wind: ${weatherItem.wind.speed} MPH</h4>
+                    <h4>Humidity: ${weatherItem.main.humidity}%</h4>    
+                </li>`;
+    }
+}
+
+// Fahrenheit = (Celsius * 9/5) + 32
+function displayCurrentWeather(data) {
+    // Extract and format the necessary information from the 'data' object
+    const cityName = data.name;
+    const date = new Date(data.dt * 1000).toLocaleDateString();
+    const celsiusTemperature = data.main.temp;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+
+    // Convert temperature to Fahrenheit
+    const fahrenheitTemperature = ((celsiusTemperature - 273.15) * 9/5) + 32;
+    const windSpeedMPH = (windSpeed * 2.24).toFixed(2);
+    // Display current weather data in the currentWeather element
+    currentWeather.innerHTML = `<h2>${cityName}</h2>
+                               <p>Date: ${date}</p>
+                               <p>Temperature: ${fahrenheitTemperature.toFixed(2)}°F</p>
+                               <p>Humidity: ${humidity}%</p>
+                               <p>Wind Speed: ${windSpeedMPH} MPH</p>`;
+}
+// // Fahrenheit = (Celsius * 9/5) + 32
+// function displayForecast(data) {
+//     // Extract and format the necessary information from the 'data' object
+//     const cityName = data.name;
+//     const date = new Date(data.dt * 1000).toLocaleDateString();
+//     const celsiusTemperature = data.main.temp;
+//     const humidity = data.main.humidity;
+//     const windSpeed = data.wind.speed;
+
+//     // Convert temperature to Fahrenheit
+//     const fahrenheitTemperature = ((celsiusTemperature - 273.15) * 9/5) + 32;
+
+//     // Display current weather data in the currentWeather element
+//     forcast.innerHTML = `<h2>${cityName}</h2>
+//                                <p>Date: ${date}</p>
+//                                <p>Temperature: ${fahrenheitTemperature.toFixed(2)}°F</p>
+//                                <p>Humidity: ${humidity}%</p>
+//                                <p>Wind Speed: ${windSpeed} MPH</p>`;
+// }
